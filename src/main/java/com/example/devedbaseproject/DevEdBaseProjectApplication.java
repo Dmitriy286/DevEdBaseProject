b5d2a7560348698670e8a5e488076771777e8567
Vadim <vadimkoyeah@gmail.com>
2022-05-25 20:47:49 +0500

    Настроенный проект без авторизации

---
+++ src/main/java/com/example/devedbaseproject/DevEdBaseProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.devedbaseproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevEdBaseProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevEdBaseProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/devedbaseproject/DevEdBaseProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devedbaseproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevEdBaseProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
