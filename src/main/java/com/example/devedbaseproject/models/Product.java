7996bc857fdb7bc08447f5c08e87c1e7c4fa845f
Vadim <vadimkoyeah@gmail.com>
2022-05-31 18:00:22 +0500

    Заменил геттеры и сеттеры аннотациями, почистил html

--- src/main/java/com/example/devedbaseproject/models/Product.java
+++ src/main/java/com/example/devedbaseproject/models/Product.java
@@ -1,40 +1,22 @@
 package com.example.devedbaseproject.models;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.*;
 
+@Getter
+@Setter
+@NoArgsConstructor
 @Entity // сущность связана с БД
 @Table(name = "products") // имя, связанной таблицы
 public class Product {
-    public Product(){}
     public Product(Long id) {
         this.id = id;
     }
     public Product(String productName, String country) {
         this.productName = productName;
-        this.country = country;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getProductName() {
-        return productName;
-    }
-
-    public void setProductName(String productName) {
-        this.productName = productName;
-    }
-
-    public String getCountry() {
-        return country;
-    }
-
-    public void setCountry(String country) {
         this.country = country;
     }
 
