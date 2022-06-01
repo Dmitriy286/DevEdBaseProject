5ea976bba73f01d1ca18c67d8ab9245d35cb88b9
Dmitry286 <dmishi@live.spbstu.ru>
2022-06-01 12:33:25 +0300

    Реализовано: - модель "Сотрудник" (Employee) - репозиторий "Сотрудник"

--- src/main/java/com/example/devedbaseproject/models/Employee.java
+++ src/main/java/com/example/devedbaseproject/models/Employee.java
@@ -1,20 +1,112 @@
 package com.example.devedbaseproject.models;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "Employees")
 public class Employee {
-    private Long ID;
 
-    public void setID(Long id) {
-        this.ID = id;
+    public Employee(){}
+
+    public Employee(Long ID, String name, String login, String password, String email, String phonenumber, String photo) {
+        this.ID = ID;
+        this.name = name;
+        this.login = login;
+        this.password = password;
+        this.email = email;
+        this.phonenumber = phonenumber;
+        this.photo = photo;
     }
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long ID;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "login")
+    private String login;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "phonenumber")
+    private String phonenumber;
+
+    @Column(name = "photo")
+    private String photo;
+
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "ID=" + ID +
+                ", name='" + name + '\'' +
+                ", login='" + login + '\'' +
+                ", password='" + password + '\'' +
+                ", email='" + email + '\'' +
+                ", phonenumber='" + phonenumber + '\'' +
+                ", photo='" + photo + '\'' +
+                '}';
+    }
 
     public Long getID() {
         return ID;
+    }
+
+    public void setID(Long ID) {
+        this.ID = ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhonenumber() {
+        return phonenumber;
+    }
+
+    public void setPhonenumber(String phonenumber) {
+        this.phonenumber = phonenumber;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
     }
 }
