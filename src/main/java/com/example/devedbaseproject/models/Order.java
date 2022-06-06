324cda35e8489bfbf46d1b91ba7845855f99ba7f
Vadim <vadimkoyeah@gmail.com>
2022-06-06 22:01:43 +0500

    initial commit

---
+++ src/main/java/com/example/devedbaseproject/models/Order.java
@@ -0,0 +1,21 @@
+package com.example.devedbaseproject.models;
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@Entity
+@Table(name="orders")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+}
