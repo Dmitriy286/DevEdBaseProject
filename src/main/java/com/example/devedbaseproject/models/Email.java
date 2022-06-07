a621bebac35252cdd081aefd827be3d6b76703ce
Dmitry286 <dmishi@live.spbstu.ru>
2022-06-07 14:45:49 +0300

    Сущность "Email" Реализована CRUD модель. Реализованы связи с сущностями Продукт, Клиент, Работник. Необходимо допилить ряд методов, в частности метод рассылки (текущая реализация - вывод информации о клиенте и электронном адресе для отправки).

---
+++ src/main/java/com/example/devedbaseproject/models/Email.java
@@ -0,0 +1,70 @@
+package com.example.devedbaseproject.models;
+
+import javax.persistence.*;
+
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+//@ToString
+@Table(name = "emails")
+public class Email {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    @Column(name = "date")
+    private String date;
+
+    @Column(name = "message")
+    private String message;
+
+    @OneToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "customerId")
+    private Customer customer;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+//    @JoinColumn(name = "products")
+    private List<Product> products;
+
+    @OneToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "employeeId")
+    private Employee employee;
+
+    @Column(name = "send")
+    private boolean send;
+
+
+    public Email(String date, String message) {
+
+        this.date = date;
+        this.message = message;
+
+        this.customer = new Customer();
+        this.products = new ArrayList<>();
+        this.employee = new Employee();
+
+        this.send = false;
+    }
+
+    @Override
+    public String toString() {
+        return "Email{" +
+                "Id=" + Id +
+                ", date='" + date + '\'' +
+                ", message='" + message + '\'' +
+                ", customer=" + customer +
+                ", products=" + products +
+                ", employee=" + employee +
+                ", send=" + send +
+                '}';
+    }
+}
+
