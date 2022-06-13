02453eeba0a671891fc0f3fa33e58b8bea3851dc
Dmitry286 <dmishi@live.spbstu.ru>
2022-06-13 19:16:18 +0300

    Вернул application.properties Ссылка "назад на главную" на вьюшке продукта. Контроллер и вьюшка Employee - refactoring

--- src/main/java/com/example/devedbaseproject/controllers/EmployeesController.java
+++ src/main/java/com/example/devedbaseproject/controllers/EmployeesController.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Controller
-@RequestMapping("/employees/")
+@RequestMapping("/employees")
 public class EmployeesController {
     private final IEmployeeRepository repository;
 
